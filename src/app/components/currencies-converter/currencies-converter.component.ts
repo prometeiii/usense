dfb122ca0e61ee533d2d3a6960a84cd1894d9f91
prometeiii <prometeii1998@gmail.com>
2024-07-18 18:11:31 +0300

    currencies-converter in work

---
+++ src/app/components/currencies-converter/currencies-converter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-currencies-converter',
+  standalone: true,
+  imports: [],
+  templateUrl: './currencies-converter.component.html',
+  styleUrl: './currencies-converter.component.css'
+})
+export class CurrenciesConverterComponent {
+
+}
